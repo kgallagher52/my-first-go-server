213d4d1e710b078dc588e05cfab1cba4e7782f27
Keaton Gallagher <d00249750@dmail.dixie.edu>
2020-01-07 14:13:28 -0700

    Established connection with the database

--- main.go
+++ main.go
@@ -1,11 +1,14 @@
 package main
 
 import (
+	"database/sql"
 	"fmt"
 	"log"
 	"net/http"
+	"os"
 
 	"github.com/gorilla/mux"
+	"github.com/lib/pq"
 )
 
 type user struct {
@@ -22,19 +25,44 @@ type error struct {
 	Message string `json-:"message"`
 }
 
+// DB variable
+var db *sql.DB
+
 func main() {
+	// Establishing a connection to the database
+	pgURL, err := pq.ParseURL(os.Getenv("ELEPHANTSQL_URL"))
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// Opening connection ("Driver", URL)
+	db, err = sql.Open("postgres", pgURL)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// Check if the connection has been established or not if ping comes back empty it has been established.
+
+	err = db.Ping()
+
 	// Declaring the router returning a new router instance
 	r := mux.NewRouter()
-	// Handling routes first the ("Route", function).REST METHOD USING
+
+	// Handling routes first the ("Route", function).Methods("REST") METHOD USING
 	r.HandleFunc("/signup", signup).Methods("POST")
 	r.HandleFunc("/signin", signin).Methods("POST")
-	// This makes a protected endpont once the JWT has been verified
+
+	// This makes a protected endpoint once the JWT has been verified
 	r.HandleFunc("/protected", tokenVerifyMiddleware(protectedEndpoint)).Methods("GET")
 
-	// log.Fatal() - Will stop the program and log the error
 	// Starting the server
 	log.Println("Listen on port 8000....")
-	log.Fatal(http.ListenAndServe(":8000", r)) // Method that takes two parameters 1. address 2. handler function
+
+	/* log.Fatal() - Will stop the program and log the error
+	Method that takes two parameters 1. address 2. handler function */
+	log.Fatal(http.ListenAndServe(":8000", r))
 }
 
 func signup(w http.ResponseWriter, r *http.Request) {
@@ -45,7 +73,6 @@ func signup(w http.ResponseWriter, r *http.Request) {
 func signin(w http.ResponseWriter, r *http.Request) {
 	fmt.Println("Signin invoked.")
 	w.Write([]byte("Successfully called signin")) // Sending a response
-
 }
 
 func protectedEndpoint(w http.ResponseWriter, r *http.Request) {
